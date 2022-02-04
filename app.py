9b7472767f3d937f006ec60e423248e6b236a819
calvi7 <jcalvi@fi.uba.ar>
2022-02-04 16:42:03 -0300

    smth

--- app.py
+++ app.py
@@ -2,9 +2,14 @@ from flask import Flask, render_template, request
 from lxml import html
 import requests
 import time
+import os
 
 app = Flask(__name__)
+
 app.static_folder = 'static'
+
+port = int(os.environ.get('PORT', 5000))
+app.run(host='0.0.0.0', port=port, debug=True)
 
 @app.route('/')
 def hello():
